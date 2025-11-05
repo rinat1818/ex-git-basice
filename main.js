0ee03c7d09c100e493398a9e360e769d2e99fae4
Rinat <ladyubaitours@gmail.com>
2025-11-05 20:09:34 +0400

    Add more css

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
- function onBallClick(){
+ 
 
-    console.log('this is a ball :)');
-    
+ 
+ function onBallClick(){
+var size =100
+size+=50
+ var elBall =  document.querySelector('.ball') 
+    elBall.style.height = size+'px'
+    elBall.style.width = size +'px'
+console.log(size);
+
  }
- console.log(':)');
+ 
  
